a4ba186c520002f296158a9c5ab7c4f20778f484
winnieotieno770 <winnieotieno770@gmail.com>
2026-01-06 15:27:56 +0300

    feat: add Teacher and Student classes with basic learning logic

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,23 @@ from user import User
 import random
 
 class Teacher(User):
+    
+    def __init__(self,first_name,last_name):
+
+        self.knowledge = [
+                        "str is a data type in Python",
+                        "programming is hard, but it's worth it",
+                        "JavaScript async web request",
+                        "Python function call definition",
+                        "object-oriented teacher instance",
+                        "programming computers hacking learning terminal",
+                        "pipenv install pipenv shell",
+                        "pytest -x flag to fail fast",
+                    ]
+        super().__init__(first_name,last_name)
+        self.first_name = first_name
+        self.last_name = last_name
 
     def teach(self):
-        pass
+        learn = random.choice(self.knowledge)
+        return learn
